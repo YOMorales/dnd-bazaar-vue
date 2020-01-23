390a73cb9f4b2caac659694325ce1b45776986ef
YOMorales <yomorales@gmail.com>
2020-01-22 22:10:28 -0600

    Deleted a lot of default stuff that Vue-CLI did, and changed index.html.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+// set this to false to prevent the production tip on Vue startup
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#container_app');
