d9b44127e2c96fe55b7190c92f5e6de4c65292e1
YOMorales <yomorales@gmail.com>
2020-01-24 21:57:23 -0600

    Added Bulma CSS to the scripts:
    +Imported Bulma inside an SCSS (sass) file.
    +Also installed sass and sass loader.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+require('./assets/css/app.scss');
+
 // set this to false to prevent the production tip on Vue startup
 Vue.config.productionTip = false;
 
