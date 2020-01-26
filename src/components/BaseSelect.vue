5d87c91c6d2003c074ab568a6c349cfdd152b424
YOMorales <yomorales@gmail.com>
2020-01-25 22:24:24 -0600

    Made new component for select form fields:
    +Hardcoded 'shop types' values for now.

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id='select_shop' class="select is-small is-info is-rounded is-gap">
+        <select v-model="selected_shop" v-on:change="loadItems">
+            <option v-for="shop in shop_types" v-bind:key="`option-${shop.value}`" v-bind:value="shop.value">{{ shop.name }}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'WidgetWeight',
+    data() {
+        return {
+            selected_shop: null,
+            shop_types: [
+                {value: 'weapons', name: 'Weapon Shop'},
+                {value: 'armor', name: 'Armory'},
+                {value: 'gear', name: 'Gear'}
+            ],
+        }
+    },
+    methods: {
+        loadItems() {
+            // had to disable this because console gave me fatal error
+            console.log('loadItems for: ' + this.selected_shop);
+        }
+    }
+}
+</script>
+
+<style>
+</style>
--- src/components/SectionWidgets.vue
+++ src/components/SectionWidgets.vue
@@ -3,6 +3,14 @@
     <div id='container_widgets' class="container">
         <div class="columns">
             <div class="column">
+                <div class="card">
+                    <div class="card-content content">
+                        <p>Lorem ipsum dolor sit amet consectetuer adipiscing elit.</p>
+                        <BaseSelect />
+                    </div>
+                </div>
+            </div>
+            <div class="column">
                 <div class="tile is-ancestor">
                     <div class="tile is-parent is-3">
                         <WidgetWeight title="Weight"/>
@@ -24,12 +32,14 @@
 </template>
 
 <script>
+import BaseSelect from './BaseSelect.vue';
 import WidgetWeight from './WidgetWeight.vue';
 import WidgetMoney from './WidgetMoney.vue';
 
 export default {
   name: 'SectionWidgets',
   components: {
+    BaseSelect,
     WidgetWeight,
     WidgetMoney
   }
