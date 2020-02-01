a23f79958abad9ee787a60ce46b095666896b8d8
YOMorales <yomorales@gmail.com>
2020-02-01 00:11:03 -0600

    Added initial code to get items from backend:
    +Imported axios.
    +Sets some vars and constants.
    +Made one API call to get items for shop, and another call for backpack
    items.

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,50 @@
 </template>
 
 <script>
+import axios from 'axios';
 import SectionWidgets from './components/SectionWidgets.vue';
+
+const rest_api_url = 'http://localhost:3000/';
 
 export default {
   name: 'App',
   components: {
       SectionWidgets
   },
-  mounted() {
+  data() {
+      return {
+         shop_items: [],
+         backpack_items: [],
+      }
   },
     methods: {
-        loadItems(selected_value) {
-            console.log('loadItems for: ' + selected_value);
-        }
+        loadItems(selected_shop) {
+            // gets items for the shop
+            axios.get(rest_api_url + selected_shop)
+                .then(response => {
+                    // puts response's items in Vue property, allowing it to render the items in the shop
+                    this.shop_items = response.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+                .finally(() => {
+                    console.log(this.shop_items);
+                });
+
+            // gets items for the backpack
+            axios.get(rest_api_url + 'bp_' + selected_shop)
+                .then(response => {
+                    // puts response's items in Vue property, allowing it to render the items in the backpack
+                    this.backpack_items = response.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+                .finally(() => {
+                    console.log(this.backpack_items);
+                });
+        },
     }
 }
 </script>
